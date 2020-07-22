a2e3cf55e6e3350af05e1ad55bf5af208271dbaa
Charles Cunningham <c.a.cunningham6@gmail.com>
2020-07-22 19:02:57 +0200

    feat(enc): impl unlock for locked wallet

--- src/locked.rs
+++ src/locked.rs
@@ -1,9 +1,31 @@
 use super::unlocked::UnlockedWallet;
+use serde::{Deserialize, Serialize};
+use serde_json::from_str;
+use ursa::{
+    encryption::symm::prelude::*,
+    hash::{sha3::Sha3_256, Digest},
+};
 
+#[derive(Serialize, Deserialize)]
 pub struct LockedWallet {
-    encrypted_data: Vec<u8>,
+    pub id: String,
+    pub ciphertext: Vec<u8>,
 }
 
 impl LockedWallet {
-    pub fn unlock(&self, key: &[u8]) -> Result<UnlockedWallet, 'str> {}
+    pub fn unlock(&self, key: &[u8]) -> Result<UnlockedWallet, String> {
+        let mut sha3 = Sha3_256::new();
+        sha3.input(key);
+        let pass = sha3.result();
+
+        let aes = SymmetricEncryptor::<Aes256Gcm>::default();
+        from_str(
+            std::str::from_utf8(
+                &aes.decrypt_easy(self.id.as_bytes(), &self.ciphertext)
+                    .map_err(|e| e.to_string())?,
+            )
+            .map_err(|e| e.to_string())?,
+        )
+        .map_err(|e| e.to_string())?
+    }
 }
