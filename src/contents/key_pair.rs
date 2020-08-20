0bbbcb965e4312e94d91a1362d0049ebd73d4f72
Ivan Temchenko <35359595i@gmail.com>
2020-08-20 11:46:49 +0200

    Error handling wrapperst and refactoring

--- src/contents/key_pair.rs
+++ src/contents/key_pair.rs
@@ -10,6 +10,7 @@ use ursa::{
 };
 use crypto::digest::Digest;
 use crypto::sha3::Sha3;
+use crate::Error;
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct KeyPair {
@@ -19,11 +20,12 @@ pub struct KeyPair {
 }
 
 impl KeyPair {
-    pub fn random_pair(key_type: KeyType) -> Result<KeyPair, String> {
+    pub fn random_pair(key_type: KeyType) -> Result<KeyPair, Error> {
         match key_type {
             KeyType::Ed25519VerificationKey2018 => {
                 let ed = Ed25519Sha512::new();
-                let (pk, sk) = ed.keypair(None).map_err(|e| e.to_string())?;
+                let (pk, sk) = ed.keypair(None)
+                    .map_err(|e| Error::UrsaCryptoError(e))?;
                 Ok(KeyPair {
                     public_key: PublicKeyInfo {
                         controller: vec![],
@@ -35,7 +37,8 @@ impl KeyPair {
             }
             KeyType::EcdsaSecp256k1VerificationKey2019 => {
                 let scp = EcdsaSecp256k1Sha256::new();
-                let (pk, sk) = scp.keypair(None).map_err(|e| e.to_string())?;
+                let (pk, sk) = scp.keypair(None)
+                    .map_err(|e| Error::UrsaCryptoError(e))?;
                 Ok(KeyPair {
                     public_key: PublicKeyInfo {
                         controller: vec![],
@@ -47,7 +50,8 @@ impl KeyPair {
             }
             KeyType::EcdsaSecp256k1RecoveryMethod2020 => {
                 let scp = EcdsaSecp256k1Sha256::new();
-                let (pk, sk) = scp.keypair(None).map_err(|e| e.to_string())?;
+                let (pk, sk) = scp.keypair(None)
+                    .map_err(|e| Error::UrsaCryptoError(e))?;
                 Ok(KeyPair {
                     public_key: PublicKeyInfo {
                         controller: vec![],
@@ -57,7 +61,7 @@ impl KeyPair {
                     private_key: sk,
                 })
             }
-            _ => Err("key type unsupported".to_string()),
+            _ => Err(Error::UnsupportedKeyType),
         }
     }
     pub fn controller(self, controller: Vec<String>) -> Self {
@@ -66,20 +70,22 @@ impl KeyPair {
             ..self
         }
     }
-    pub fn sign(&self, data: &[u8]) -> Result<Vec<u8>, String> {
+    pub fn sign(&self, data: &[u8]) -> Result<Vec<u8>, Error> {
         match self.public_key.key_type {
             KeyType::Ed25519VerificationKey2018 => {
                 let ed = Ed25519Sha512::new();
-                ed.sign(data, &self.private_key).map_err(|e| e.to_string())
+                ed.sign(data, &self.private_key)
+                    .map_err(|e| Error::UrsaCryptoError(e))
             }
             KeyType::EcdsaSecp256k1VerificationKey2019 => {
                 let scp = EcdsaSecp256k1Sha256::new();
-                scp.sign(data, &self.private_key).map_err(|e| e.to_string())
+                scp.sign(data, &self.private_key)
+                    .map_err(|e| Error::UrsaCryptoError(e))
             }
             KeyType::EcdsaSecp256k1RecoveryMethod2020 => {
                 let scp = Secp256k1::new();
                 let secp_secret_key = SecretKey::from_slice(&self.private_key.0)
-                    .map_err(|e| e.to_string())?;
+                    .map_err(|e| Error::SecpCryptoError(e))?;
 
                 let mut hasher = Sha3::keccak256();
                 hasher.input(data);
@@ -87,7 +93,7 @@ impl KeyPair {
                 hasher.result(&mut output);
 
                 let message = Message::from_slice(&output)
-                    .map_err(|e| e.to_string())?;
+                    .map_err(|e| Error::SecpCryptoError(e))?;
 
                 let sig = scp.sign_recoverable(&message, &secp_secret_key);
                 let (rec_id, rs) = sig.serialize_compact();
@@ -99,10 +105,10 @@ impl KeyPair {
 
                 Ok(ret)
             }
-            _ => Err("wrong key type".to_string()),
+            _ => Err(Error::WrongKeyType),
         }
     }
-    pub fn decrypt(&self, data: &[u8], aad: &[u8]) -> Result<Vec<u8>, String> {
+    pub fn decrypt(&self, data: &[u8], _aad: &[u8]) -> Result<Vec<u8>, Error> {
         match self.public_key.key_type {
             // default use xChaCha20Poly1905 with x25519 key agreement
             KeyType::X25519KeyAgreementKey2019 => unseal_box::<X25519Sha256, XChaCha20Poly1305>(
@@ -110,7 +116,7 @@ impl KeyPair {
                 &self.public_key.public_key,
                 &self.private_key,
             ),
-            _ => Err("wrong key type".to_string()),
+            _ => Err(Error::WrongKeyType),
         }
     }
     pub fn clean(&self) -> PublicKeyInfo {
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
+extern crate aead;
+
 pub mod contents;
 pub mod locked;
 pub mod unlocked;
 
+use thiserror::Error as ThisError;
 use ursa::encryption::random_vec;
 
-pub fn get_random(len: usize) -> Result<Vec<u8>, String> {
-    random_vec(len).map_err(|e| e.to_string())
+pub fn get_random(len: usize) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    Ok(random_vec(len).map_err(|e| Error::AeadCryptoError(e))?)
 }
 
 pub mod prelude {
@@ -18,23 +21,63 @@ pub mod prelude {
     pub use crate::unlocked::UnlockedWallet;
 }
 
+/// Wrapper enum for proper error handling
+#[derive(Debug, ThisError)]
+pub enum Error {
+    /// Indicates error during key insertion
+    #[error("error inserting key")]
+    KeyInsertionError,
+    /// Type of used key is not supported in this context
+    #[error("key type unsupported")]
+    UnsupportedKeyType,
+    /// Type of used key is invalid in this context
+    #[error("key type wrong")]
+    WrongKeyType,
+    /// No key found
+    #[error("key not found")]
+    KeyNotFound,
+    /// Content type is incorrect in current context
+    #[error("incorrect content type")]
+    ContentTypeIncorrect,
+    /// Opaque errors wrapper for aead crate
+    #[error("cryptography failure in aead: {0}")]
+    AeadCryptoError(aead::Error),
+    /// Opaque errors wrapper for ursa crate
+    #[error("cryptography failure in ursa: {0}")]
+    UrsaCryptoError(ursa::CryptoError),
+    /// Opaque errors wrapper for secp256k1 crate
+    #[error("cryptography failure in secp256k1: {0}")]
+    SecpCryptoError(secp256k1::Error),
+    /// #Transparent errors
+    ///
+    /// Serde crate errors
+    #[error(transparent)]
+    Serde(#[from] serde_json::error::Error),
+    /// utf8 conversion errors
+    #[error(transparent)]
+    Utf8(#[from] std::str::Utf8Error),
+    /// Other errors implementing `std::error::Error`
+    #[error(transparent)]
+    Other(#[from] Box<dyn std::error::Error>)
+}
+
 #[cfg(test)]
 mod tests {
     use crate::prelude::*;
+    use super::Error;
 
     #[test]
-    fn secp256k1_recoverable_round_trip() -> Result<(), String> {
+    fn secp256k1_recoverable_round_trip() -> Result<(), Error> {
         let message = "hello".as_bytes();
         let mut w = UnlockedWallet::new("thing");
         let pk_info = w.new_key(KeyType::EcdsaSecp256k1RecoveryMethod2020, None)?;   
 
         let sig = w.sign_raw(&pk_info.id, &message)?;
 
-        assert_eq!(
-            Ok(true),
+        assert!(
             match pk_info.content {
-                Content::PublicKey(r_pk_inf) => r_pk_inf.verify(&message, &sig),
-                _ => Ok(false),
+                Content::PublicKey(r_pk_inf) => r_pk_inf.verify(&message, &sig)?,
+                _ => false,
             }
         );
         Ok(())
