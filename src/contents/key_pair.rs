5ea0e039c994eec3351df5cc9abe4d1f7dcb4d66
Charles Cunningham <c.a.cunningham6@gmail.com>
2021-01-25 17:04:56 +0100

    test(ecdh): add rfc7748 x25519 ecdh test vectors

--- src/contents/key_pair.rs
+++ src/contents/key_pair.rs
@@ -299,3 +299,39 @@ fn key_pair_new_ecdsa_x25519() -> Result<(), Error> {
     assert_eq!(key_entry.public_key.public_key, expected_pk);
     Ok(())
 }
+
+#[test]
+fn ecdh_test_1() -> Result<(), Error> {
+    // test vector from https://tools.ietf.org/html/rfc7748#section-5.2
+    let sk =
+        hex::decode("a546e36bf0527c9d3b16154b82465edd62144c0ac1fc5a18506a2244ba449ac4").unwrap();
+
+    let pk =
+        hex::decode("e6db6867583030db3594c1a424b15f7c726624ec26b3353b10a903a6d0ab1c4c").unwrap();
+
+    let ak =
+        hex::decode("c3da55379de9c6908e94ea4df28d084f32eccf03491c71f754b4075577a28552").unwrap();
+
+    let test_kp = KeyPair::new(KeyType::X25519KeyAgreementKey2019, &sk)?;
+
+    assert_eq!(test_kp.ecdh_key_agreement(&pk)?, ak);
+    Ok(())
+}
+
+#[test]
+fn ecdh_test_2() -> Result<(), Error> {
+    // test vector from https://tools.ietf.org/html/rfc7748#section-5.2
+    let sk =
+        hex::decode("4b66e9d4d1b4673c5ad22691957d6af5c11b6421e0ea01d42ca4169e7918ba0d").unwrap();
+
+    let pk =
+        hex::decode("e5210f12786811d3f4b7959d0538ae2c31dbe7106fc03c3efc4cd549c715a493").unwrap();
+
+    let ak =
+        hex::decode("95cbde9476e8907d7aade45cb4b873f88b595a68799fa152e6f8f7647aac7957").unwrap();
+
+    let test_kp = KeyPair::new(KeyType::X25519KeyAgreementKey2019, &sk)?;
+
+    assert_eq!(test_kp.ecdh_key_agreement(&pk)?, ak);
+    Ok(())
+}
